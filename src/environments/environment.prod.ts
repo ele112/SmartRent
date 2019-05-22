61c9cab82b83d90f6d382ab4ee24a9e226d1a55c
alfonso <alfonso@wypper.cl>
2019-05-22 11:08:27 -0400

    comenzando con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBIN7myDOyd-IL-VDhxMWAj1rhDr9vX3Hc",
+    authDomain: "duocproyecto-2a62e.firebaseapp.com",
+    databaseURL: "https://duocproyecto-2a62e.firebaseio.com",
+    projectId: "duocproyecto-2a62e",
+    storageBucket: "duocproyecto-2a62e.appspot.com",
+    messagingSenderId: "739242294973",
+    appId: "1:739242294973:web:4705d7858e63b52b"
+  }
 };
